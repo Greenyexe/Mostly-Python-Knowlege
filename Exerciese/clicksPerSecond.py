2700dd4596b54046d3ee839f5016295922a83322
Bruno Greenfield <21greenfieldb@thomas-hardye.net>
2022-05-26 16:41:25 +0100

    l

--- Exerciese/clicksPerSecond.py
+++ Exerciese/clicksPerSecond.py
@@ -7,9 +7,18 @@ count = 0
 
 def click():
   global count
+  count += 1
+  time.sleep(1)
 
 window = Tk()
 
 button = Button(window,
                 text="Click me as fast as you can!",
-                command=click,)
+                command=click,
+                fg="#00FF00",
+                bg="black",
+                font=("Times New Roman", 40))
+
+button.pack()
+
+window.mainloop()
