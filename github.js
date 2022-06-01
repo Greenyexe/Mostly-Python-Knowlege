774bc38132fcfc634abae65a06d3e9fe3c8822d9
Bruno Greenfield <21greenfieldb@thomas-hardye.net>
2022-06-01 13:18:20 +0100

    new files!

---
+++ github.js
@@ -0,0 +1,19 @@
+function calculateDatsBetweenDates(begin, end) {
+  var beginDate = new Date(begin);
+  var endDate = new Date(end);
+  var millisecondsPerDay = 1000 * 60 * 60 * 24;
+  var millisBetween = endDate.getTime() - beginDate.getTime();
+  var days = millisBetween / millisecondsPerDay;
+  return Math.floor(days);
+}
+
+// find all images without alternate text
+// and give them a red border
+function thing() {
+  var images = document.getElementsByTagName('img');
+  for (var i = 0; i < images.length; i++) {
+    if (images[i].alt == '') {
+      images[i].style.border = '3px solid red';
+    }
+  }
+}
