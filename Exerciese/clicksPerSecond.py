661d37c0aff2029d5893bfa966d2e7414896f92f
Bruno Greenfield <21greenfieldb@thomas-hardye.net>
2022-06-08 12:15:49 +0100

    f

--- Exerciese/clicksPerSecond.py
+++ Exerciese/clicksPerSecond.py
@@ -3,22 +3,55 @@
 from tkinter import *
 import time
 
+#*****************************************************************************************
+
+def callback():
+  global timey
+  timey = int(entrybox.get())
+  print(timey)
+  window.after(timey*1000, lambda: window.destroy())
+
+#*****************************************************************************************
+
 count = []
-time = int(input("How long do you want to have to click for (in seconds)?\n: "))
+
+#*****************************************************************************************
 
 window = Tk()
 
+#*****************************************************************************************
+
+entrybox = Entry(window,
+                 width=15,
+                 bg="pink",
+                 fg="black",
+                 font=("Arial", 50))
+entrybox.pack()
+
+#*****************************************************************************************
+
+submit = Button(window,
+                text="Submit",
+                command=callback,
+                font=("Arial", 20),
+                bg="green",
+                fg="black")
+submit.pack()
+
+#*****************************************************************************************
+
 button = Button(window,
-                text="Click me as fast\nas you can!",
+                text="Click me as fast\nas you can after\ninputing the time\non the entrybox above!",
                 command=lambda: count.append(1),
                 fg="#00FF00",
                 bg="black",
                 font=("Times New Roman", 60))
-
 button.pack()
 
-window.after(time*1000, lambda: window.destroy())
+#*****************************************************************************************
 
 window.mainloop()
 
-print("{} clicks in {} seconds".format(len(count), time))
+#*****************************************************************************************
+
+print("{} clicks in {} seconds".format(len(count), timey))
