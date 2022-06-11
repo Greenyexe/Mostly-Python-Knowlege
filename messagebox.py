1ec528d461cb9258e10536c67fb2c2568f4b7d78
Bruno Greenfield <21greenfieldb@thomas-hardye.net>
2022-06-11 21:28:54 +0100

    ye

--- messagebox.py
+++ messagebox.py
@@ -6,10 +6,31 @@ def click():
 #  while True:
 #   messagebox.showwarning(title = "Warning!", message = "You have a virus")
 #   messagebox.showerror(title = "Error!", message = "You have an error")
-  if messagebox.askokcancel(title = "Are you sure?", message = "Are you sure you want to quit?"):
-    print("You clicked ok")
+#  if messagebox.askokcancel(title = "Are you sure?", message = "Are you sure you want to quit?"):
+#    print("You clicked ok")
+#  else:
+#    print("You clicked cancel")
+#  if messagebox.askretrycancel(title = "Retry?", message = "Do you want to retry?"):
+#    print("You clicked retry")
+#  else:
+#    print("You clicked cancel")
+#  if messagebox.askyesno(title = "Yes or No?", message = "Do you want to continue?"):
+#    print("You clicked yes")
+#  else:
+#    print("You clicked no")
+#  answer = messagebox.askquestion(title = "Question", message = "Do you want to continue?")
+#  if answer == "yes":
+#    print("You clicked yes")
+#  else:
+#    print("You clicked no")
+  answer = messagebox.askyesnocancel(title = "Question", message = "Do you like to code?",icon = "error")
+  if answer==True:
+    print("You like to code")
+  elif answer==False:
+    print("You don't like to code")
   else:
-    print("You clicked cancel")
+    print("You don't want to answer")
+
 
 window = Tk()
 
