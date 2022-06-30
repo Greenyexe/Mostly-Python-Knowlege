429fdbcbb600e0d21a3f90fce7e94be828b64a21
Bruno Greenfield <21greenfieldb@thomas-hardye.net>
2022-06-30 21:29:12 +0100

    new files

---
+++ Exerciese/dad.py
@@ -0,0 +1,3 @@
+word = list(reversed(input("Enter a word: ")))
+for i in word:
+  print(i, end="")
---
+++ Exerciese/joesWordChallenge.py
@@ -0,0 +1,30 @@
+import random
+
+words=["apple","banana","cherry","orange","kiwi","lemon","grapes","mango", "peach","pineapple","strawberry", "watermelon", "blueberry", "blackberry", "raspberry"]
+random.shuffle(words)
+word=words[0]
+
+vowels = ["a", "e", "i", "o", "u"]
+line_letters = ["a","e","f","h","i","k","l","m","n","t","v","w","x","y","z"]
+enclosed_letters = ["a","b","d","o","p","q"]
+
+vowel_count = 0
+line_count = 0
+enclosed_count = 0
+
+for i in word:
+  if i in vowels:
+    vowel_count +=1
+for i in word:
+  if i in line_letters:
+    line_count +=1
+for i in word:
+  if i in enclosed_letters:
+    enclosed_count +=1
+guess = input(f"There are {vowel_count} vowels, {line_count} line letters, and {enclosed_count} enclosed letters\nGuess the word: ")
+
+if guess == word:
+  print("You win!")
+else:
+  print("You lose!")
+  print(f"The word was {word}")
