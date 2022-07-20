ed41491d50db4784d8b49803790102295d9eb074
Bruno Greenfield <21greenfieldb@thomas-hardye.net>
2022-07-20 21:13:45 +0100

    new file!

---
+++ Exerciese/annoyDadCauseImBored.py
@@ -0,0 +1,6 @@
+from tkinter import messagebox
+
+messagebox.showerror(title="Error", message="Zero brain cells found")
+
+while True:
+    messagebox.showwarning(title="Sorry", message="Did this just to annoy you a bit I apologize")
